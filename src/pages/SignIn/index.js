2c3950c4838e8576449310e62662b857610a5f32
Caio Yoshida <caio.yoshida@yahoo.com.br>
2019-12-08 23:06:31 -0300

    chore: auth Styling + polished

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
 
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <>
+      <img src={logo} alt="gobarber" />
+
+      <form action="">
+        <input type="email" placeholder="Your e-mail" />
+        <input type="password" placeholder="Password" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Create your Free Account Now!</Link>
+      </form>
+    </>
+  );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
 
 // import { Container } from './styles';
 
 export default function SignUp() {
-  return <h1>SignUp</h1>;
+  return (
+    <>
+      <img src={logo} alt="gobarber" />
+
+      <form action="">
+        <input placeholder="Entire Name" />
+        <input type="email" placeholder="Your e-mail" />
+        <input type="password" placeholder="Password" />
+
+        <button type="submit">Create Account</button>
+        <Link to="/">I'm already registered</Link>
+      </form>
+    </>
+  );
 }
