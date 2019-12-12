a8b1e8a9a2960a0e48993524a94d4e8535ced40d
Caio Yoshida <caio.yoshida@yahoo.com.br>
2019-12-12 12:51:32 -0300

    chore: creating profile's page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Full Name" />
+        <Input name="email" type="email" placeholder="Your e-mail adress" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Your current password"
+        />
+        <Input type="password" name="Password" placeholder="New Password" />
+        <Input
+          type="password"
+          name="ConfirmPassword"
+          placeholder="New Password's Confirmation"
+        />
+
+        <button type="submit">Atualizar Perfil</button>
+      </Form>
+
+      <button type="button">Log out</button>
+    </Container>
+  );
 }
