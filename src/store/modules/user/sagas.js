6525e599365af25fd421ffc3bab13ae0d65149b5
Caio Yoshida <caio.yoshida@yahoo.com.br>
2019-12-12 13:27:53 -0300

    chore: updating profile configuration

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,3 +1,31 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest, call, put } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all({});
+import api from '~/services/api';
+
+import {
+  updateProfileSuccess,
+  updateProfileFailure,
+} from '~/store/modules/user/actions';
+
+export function* updateProfile({ payload }) {
+  const { name, email, ...rest } = payload.data;
+
+  // we use "Object.assign() is used to merge two or more obejcts"
+  const profile = { name, email, ...(rest.oldPassword ? rest : {}) };
+
+  try {
+    const response = yield call(api.put, 'users', profile);
+
+    toast.success('Profile was updated successfully');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (err) {
+    toast.error(
+      `Something went wrong on profile's update. Check your informations`
+    );
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
